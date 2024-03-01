14a22977c52b8076878d70e7e6c810a3e08f25bb
taoc41 <146112761+taoc41@users.noreply.github.com>
2024-03-01 23:58:19 +0000

    a

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
-function setup(){
-    createCanvas(500,500)
+let ball;
+
+function setup() {
+	new Canvas(500, 500);
+
+	ball = new Sprite();
+	ball.diameter = 50;
 }
 
-function preload(){
-    
+function draw() {
+	background('gray');
 }
-
-function draw(){
-
-}
-
