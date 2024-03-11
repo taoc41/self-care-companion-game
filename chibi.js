9e5c1728505074dd6955076929f7073a176617d1
taoc41 <146112761+taoc41@users.noreply.github.com>
2024-03-11 13:28:39 +0000

    a

--- chibi.js
+++ chibi.js
@@ -1,1 +1,17 @@
+class Chibi {
+    #sprite
 
+    static IDLE = 10;
+    static WALKING_LEFT = 20;
+    static WALKING_RIGHT = 21;
+    static PICK_UP = 30;
+    static FALLING = 31
+    static STAND = 32
+
+    constructor() {
+        this.
+        this.#sprite = new Sprite(width/2, 0 - 100);
+    }
+
+    animationLoad()
+}
